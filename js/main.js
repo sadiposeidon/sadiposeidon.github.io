53eaaf4a3bff0f3bd9f4c02f49766ef0735ef2e1
sadiposeidon <38244032+sadiposeidon@users.noreply.github.com>
2024-06-24 21:44:48 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,52 +21,52 @@
 // });
 
 
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            return;
-        }
-    }
-}
+// function includeHTML() {
+//     var z, i, elmnt, file, xhttp;
+//     z = document.getElementsByTagName("*");
+//     for (i = 0; i < z.length; i++) {
+//         elmnt = z[i];
+//         file = elmnt.getAttribute("w3-include-html");
+//         if (file) {
+//             xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function() {
+//                 if (this.readyState == 4) {
+//                     if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+//                     if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+//                     elmnt.removeAttribute("w3-include-html");
+//                     includeHTML();
+//                 }
+//             }
+//             xhttp.open("GET", file, true);
+//             xhttp.send();
+//             return;
+//         }
+//     }
+// }
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
+// function scrollToTop() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
 
-window.onscroll = function() {
-    scrollFunction();
-};
+// window.onscroll = function() {
+//     scrollFunction();
+// };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector('.scroll-to-top').style.display = "block";
-    } else {
-        document.querySelector('.scroll-to-top').style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.querySelector('.scroll-to-top').style.display = "block";
+//     } else {
+//         document.querySelector('.scroll-to-top').style.display = "none";
+//     }
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const backgroundTextDiv = document.getElementById("background-text");
-    if (backgroundTextDiv) {
-        backgroundTextDiv.textContent = "@sadiposeidon";
-    }
-    document.title = "@sadiposeidon | Java";
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const backgroundTextDiv = document.getElementById("background-text");
+//     if (backgroundTextDiv) {
+//         backgroundTextDiv.textContent = "@sadiposeidon";
+//     }
+//     document.title = "@sadiposeidon | Java";
+// });
